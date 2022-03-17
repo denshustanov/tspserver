9a77b06c000cabde8d35dcabb2ce5903fefbd4eb
denshustanov <den.shustanov@gmail.com>
2022-03-17 23:57:00 +0400

    user role assignments, password encryption in user-edit

--- src/main/java/com/company/tspserver/TspserverApplication.java
+++ src/main/java/com/company/tspserver/TspserverApplication.java
@@ -8,15 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
-public class TspserverApplication {
+@PropertySource("classpath:application.properties")
+public class TspserverApplication extends SpringBootServletInitializer {
 
     @Autowired
     private Environment environment;
--- src/main/java/com/company/tspserver/screen/user/UserEdit.java
+++ src/main/java/com/company/tspserver/screen/user/UserEdit.java
@@ -1,10 +1,26 @@
 package com.company.tspserver.screen.user;
 
+import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import com.company.tspserver.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @UiController("User.edit")
 @UiDescriptor("user-edit.xml")
 @EditedEntityContainer("userDc")
 public class UserEdit extends StandardEditor<User> {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private InstanceContainer<User> userDc;
+
+    @Subscribe
+    public void onBeforeCommitChanges(BeforeCommitChangesEvent event) {
+        User user = userDc.getItem();
+        String password = user.getPassword();
+
+        user.setPassword(passwordEncoder.encode(password));
+    }
 }
